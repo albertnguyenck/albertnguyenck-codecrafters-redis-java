21b42918150a0aa7bd7f593e534c4238fdf53e2e
Albert <albert.nguyen.ck@gmail.com>
2025-12-28 11:00:45 +0800

    add PONG response

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,11 @@ void main(String[] args) {
     try (var serverSocket = new ServerSocket(6379)) {
         System.out.println("Server started on port 6379");
         serverSocket.setReuseAddress(true);
-        serverSocket.accept();
+        Socket client = serverSocket.accept();
+        try (var out = client.getOutputStream()) {
+            out.write("+PONG\r\n".getBytes(StandardCharsets.UTF_8));
+            out.flush();
+        }
     } catch (IOException e) {
         System.out.println("IOException: " + e.getMessage());
     }
