17a834a81f1d1132ffbbe1f200163ebe17a73b1c
Albert <albert.nguyen.ck@gmail.com>
2025-12-28 11:28:24 +0800

    feat: enable client to send multiple commands

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,11 +3,32 @@ void main(String[] args) {
         System.out.println("Server started on port 6379");
         serverSocket.setReuseAddress(true);
         Socket client = serverSocket.accept();
-        try (var out = client.getOutputStream()) {
-            out.write("+PONG\r\n".getBytes(StandardCharsets.UTF_8));
-            out.flush();
-        }
+        handleClient(client);
     } catch (IOException e) {
         System.out.println("IOException: " + e.getMessage());
+    }
+}
+
+void handleClient(Socket client) {
+    try (var in = client.getInputStream(); var out = client.getOutputStream()) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
+        while (true) {
+            String line = reader.readLine();
+            if (line == null) {
+                break;
+            }
+
+            if (line.equals("*1")) {
+                reader.readLine();
+                String command = reader.readLine();
+                switch (command) {
+                    case "PING" -> out.write("+PONG\r\n".getBytes(StandardCharsets.UTF_8));
+                    default -> System.out.println("Unknown command: " + command);
+                }
+                out.flush();
+            }
+        }
+    } catch (IOException e) {
+        System.out.println("IOException while handling client: " + e.getMessage());
     }
 }
